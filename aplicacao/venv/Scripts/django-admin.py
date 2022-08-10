021aba7bb3c166c2e2f5f59a2c5d41c37f65ec31
alexaugusto23 <contato.alexaugusto@hotmail.com>
2022-08-10 16:06:10 -0300

    criando urls

---
+++ aplicacao/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alexsandro.ignacio\onedrive - wca soluções de inteligência comercial\documentos\docs\git\django_alura\aplicacao\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
