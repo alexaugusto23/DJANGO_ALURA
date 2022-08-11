8ada4011664992df21456d77c10916b587c8d959
alexaugusto23 <contato.alexaugusto@hotmail.com>
2022-08-11 15:04:29 -0300

    conectando com banco.

--- aplicacao/receitas/views.py
+++ aplicacao/receitas/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    receitas = {
+        1:'Lasanha',
+        2:'Sopa de Legumes',
+        3:'Sorvete',
+        4:'Picadinho',
+        5:'Baião de Dois',
+        6:'Filé Mingnon'}
+    dados = {
+        'nome_das_receitas':receitas
+    }
+    return render(request,'index.html', dados )
 
 def receita(request):
     return render(request,'receita.html')
